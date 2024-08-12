f9253be751b30db0a6857b615e9dc8bc92713eb1
xP-xD <tlsqlfn842@naver.com>
2024-08-12 19:57:17 +0900

    feat: admin 디자인 끝

---
+++ lib/admin/complaint/notice_update_page.dart
@@ -0,0 +1,238 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+class NoticeUpdatePage extends StatelessWidget {
+  NoticeUpdatePage({super.key});
+
+  final TextEditingController titleController = TextEditingController();
+  final TextEditingController contentController = TextEditingController();
+
+  Future<void> postNotice(BuildContext context) async {
+    String title = titleController.text;
+    String content = contentController.text;
+
+    String url = 'http://localhost:8080/notice';
+
+    try {
+      final response = await http.post(
+        Uri.parse(url),
+        headers: <String, String>{
+          'Content-Type': 'application/json; charset=UTF-8',
+        },
+        body: jsonEncode(<String, String>{
+          'title': title,
+          'content': content,
+        }),
+      );
+
+      if (response.statusCode == 201) {
+        print('공지 등록 성공');
+      } else {
+        print('공지 등록 실패: ${response.statusCode}');
+        // Show the error message to the user
+        showDialog(
+          context: context,
+          builder: (BuildContext context) {
+            return AlertDialog(
+              title: Text('오류'),
+              content: Text("제목, 내용을 채워 주세요."),
+              actions: <Widget>[
+                TextButton(
+                  child: Text('확인'),
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                ),
+              ],
+            );
+          },
+        );
+      }
+    } catch (e) {
+      print('공지 등록 오류: $e');
+      // Show a general error message to the user
+      showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text('오류'),
+            content: Text('서버와의 연결 오류가 발생했습니다.'),
+            actions: <Widget>[
+              TextButton(
+                child: Text('확인'),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+              ),
+            ],
+          );
+        },
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back),
+          color: Color(0xFFFFE9F8),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+        centerTitle: false,
+        flexibleSpace: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment(0.68, -0.73),
+              end: Alignment(-0.68, 0.73),
+              colors: const [Color(0xFFFFA7E1), Color(0xB29322CC)],
+            ),
+          ),
+        ),
+      ),
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        padding: const EdgeInsets.all(12),
+        child: Column(
+          children: [
+            Expanded(
+              child: Container(
+                width: double.infinity,
+                padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 12),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  boxShadow: [
+                    BoxShadow(
+                      color: Color(0x142E1727),
+                      blurRadius: 10,
+                      offset: Offset(3, 4),
+                      spreadRadius: 0,
+                    )
+                  ],
+                ),
+                child: SingleChildScrollView(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      SizedBox(
+                        width: double.infinity,
+                        height: 20,
+                        child: Text(
+                          '제목',
+                          style: TextStyle(
+                            color: Color(0xFF5F5F5F),
+                            fontSize: 14,
+                            fontFamily: 'Inter',
+                            fontWeight: FontWeight.w400,
+                          ),
+                        ),
+                      ),
+                      const SizedBox(height: 6),
+                      TextField(
+                        decoration: InputDecoration(
+                          hintText: '제목 입력',
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(8),
+                            borderSide: BorderSide(color: Color(0xFF9932CC)),
+                          ),
+                          contentPadding: EdgeInsets.all(10),
+                        ),
+                        controller: titleController,
+                      ),
+                      const SizedBox(height: 12),
+                      SizedBox(
+                        width: double.infinity,
+                        height: 20,
+                        child: Text(
+                          '내용',
+                          style: TextStyle(
+                            color: Color(0xFF5F5F5F),
+                            fontSize: 14,
+                            fontFamily: 'Inter',
+                            fontWeight: FontWeight.w400,
+                          ),
+                        ),
+                      ),
+                      const SizedBox(height: 6),
+                      TextField(
+                        maxLines: 20, // 여러 줄 입력 가능
+                        decoration: InputDecoration(
+                          hintText: '내용 입력',
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(8),
+                            borderSide: BorderSide(color: Color(0xFF9932CC)),
+                          ),
+                          contentPadding: EdgeInsets.all(10),
+                        ),
+                        controller: contentController,
+                      ),
+                      const SizedBox(height: 20), // 하단 여백 추가
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+      bottomNavigationBar: Container(
+        width: double.infinity,
+        height: 70,
+        padding: const EdgeInsets.all(9),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          boxShadow: [
+            BoxShadow(
+              color: Color(0x14737373),
+              blurRadius: 4,
+              offset: Offset(0, -4),
+              spreadRadius: 0,
+            ),
+          ],
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Expanded(
+              child: Material(
+                color: Colors.transparent,
+                child: InkWell(
+                  onTap: () {
+                    postNotice(context);
+                  },
+                  splashColor: Colors.white.withOpacity(0.2),
+                  child: Container(
+                    height: double.infinity,
+                    padding: const EdgeInsets.all(10),
+                    decoration: ShapeDecoration(
+                      color: Color(0xFFB34FD1),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                    child: Center(
+                      child: Text(
+                        '수정하기',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 17,
+                          fontFamily: 'Inter',
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/admin/home/home_page_FA.dart
+++ lib/admin/home/home_page_FA.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:mogu_app/admin/complaint/complaint_detail_page.dart';
+import 'package:mogu_app/admin/complaint/notice_create_page.dart';
+import 'package:mogu_app/admin/complaint/notice_detail_page.dart';
 
 import '../../user/home/post/post_detail_page.dart';
 
@@ -182,6 +185,96 @@ class _HomePageFAState extends State<HomePageFA> with SingleTickerProviderStateM
     return results;
   }
 
+  List<Widget> _buildNoticeList() {
+    // 공지 탭에서 표시할 공지 목록을 정의합니다.
+    List<Map<String, String>> notices = [
+      {
+        'date': '2024/07/20',
+        'title': '2024년 7월 앱 기능 업데이트 소식 안내',
+        'description': '2024년 7월 업데이트되는 기능입니다.',
+        'likes': '700',
+        'views': '2300',
+      },
+      {
+        'date': '2024/07/20',
+        'title': '2024년 7월 앱 기능 업데이트 소식 안내',
+        'description': '2024년 7월 업데이트되는 기능입니다.',
+        'likes': '700',
+        'views': '2300',
+      },
+      {
+        'date': '2024/07/20',
+        'title': '2024년 7월 앱 기능 업데이트 소식 안내',
+        'description': '2024년 7월 업데이트되는 기능입니다.',
+        'likes': '700',
+        'views': '2300',
+      },
+    ];
+
+    // 검색어가 있으면 필터링
+    if (_searchQuery.isNotEmpty) {
+      notices = notices
+          .where((notice) => notice['title']!.contains(_searchQuery))
+          .toList();
+    }
+
+    // 필터링된 공지 목록을 위젯으로 변환
+    List<Widget> results = notices.map((notice) {
+      return _buildNoticeCard(
+        date: notice['date']!,
+        title: notice['title']!,
+        description: notice['description']!,
+        likes: notice['likes']!,
+        views: notice['views']!,
+      );
+    }).toList();
+
+    return results;
+  }
+
+  List<Widget> _buildInquiryList() {
+    // 문의 탭에서 표시할 문의 목록을 정의합니다.
+    List<Map<String, String>> inquiries = [
+      {
+        'profileName': '김찬',
+        'inquiry': '이상한 남자가 말 걸어요',
+        'imagePath': 'icon',
+        'status': '대기중',
+      },
+      {
+        'profileName': '나하리',
+        'inquiry': '개명했습니다',
+        'imagePath': '',
+        'status': '완료',
+      },
+      {
+        'profileName': '모비팡',
+        'inquiry': '모비팡모비팡모비팡모비팡모비팡모비팡',
+        'imagePath': '',
+        'status': '완료',
+      },
+    ];
+
+    // 검색어가 있으면 필터링
+    if (_searchQuery.isNotEmpty) {
+      inquiries = inquiries
+          .where((inquiry) => inquiry['inquiry']!.contains(_searchQuery))
+          .toList();
+    }
+
+    // 필터링된 문의 목록을 위젯으로 변환
+    List<Widget> results = inquiries.map((inquiry) {
+      return _buildInquiryCard(
+        profileName: inquiry['profileName']!,
+        inquiry: inquiry['inquiry']!,
+        imagePath: inquiry['imagePath']!,
+        status: inquiry['status']!,
+      );
+    }).toList();
+
+    return results;
+  }
+
   @override
   Widget build(BuildContext context) {
     // 드롭다운 값이 옵션 리스트에 포함되어 있는지 확인
@@ -275,7 +368,7 @@ class _HomePageFAState extends State<HomePageFA> with SingleTickerProviderStateM
             padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
             child: Align(
               alignment: Alignment.centerRight,
-              child: Container(
+              child: SizedBox(
                 width: 110, // 드롭다운의 고정된 너비를 설정
                 child: DropdownButtonHideUnderline(
                   child: DropdownButton<String>(
@@ -321,6 +414,21 @@ class _HomePageFAState extends State<HomePageFA> with SingleTickerProviderStateM
               padding: EdgeInsets.all(8),
               children: _buildMemberList(),
             )
+                : _tabController?.index == 0
+                ? ListView(
+              padding: EdgeInsets.all(8),
+              children: _buildInquiryList(),
+            )
+                : _tabController?.index == 1
+                ? ListView(
+              padding: EdgeInsets.all(8),
+              children: _buildSearchResults(),
+            )
+                : _tabController?.index == 2
+                ? ListView(
+              padding: EdgeInsets.all(8),
+              children: _buildNoticeList(),
+            )
                 : _buildTabContent(),
           ),
         ],
@@ -332,7 +440,12 @@ class _HomePageFAState extends State<HomePageFA> with SingleTickerProviderStateM
           child: InkWell(
             splashColor: Colors.white.withOpacity(0.3), // 물결 효과 색상
             onTap: () {
-              print('플로팅 액션 버튼 클릭됨');
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => NoticeCreatePage(),
+                ), // PostCreatePage로 이동
+              );
             },
             child: SizedBox(
               width: 56,
@@ -419,7 +532,7 @@ class _HomePageFAState extends State<HomePageFA> with SingleTickerProviderStateM
       // 민원관리 탭
       return TabBarView(
         controller: _tabController,
-        children: [
+        children: const [
           Center(child: Text('문의 내용 표시')),
           Center(child: Text('신고 내용 표시')),
           Center(child: Text('공지 내용 표시')),
@@ -526,7 +639,7 @@ class _HomePageFAState extends State<HomePageFA> with SingleTickerProviderStateM
                   ),
                   Spacer(),
                   Column(
-                    children: [
+                    children: const [
                       Icon(Icons.image, size: 56), // 임시 아이콘 사용
                       SizedBox(height: 4),
                       Text(
@@ -574,7 +687,7 @@ class _HomePageFAState extends State<HomePageFA> with SingleTickerProviderStateM
   }) {
     return InkWell(
       onTap: () {
-        print('$profileName 카드 클릭됨');
+        print('$profileName 회원 클릭됨');
       },
       splashColor: Colors.purple.withOpacity(0.3), // 물결 효과 색상
       child: Card(
@@ -632,7 +745,69 @@ class _HomePageFAState extends State<HomePageFA> with SingleTickerProviderStateM
                   ),
                   TextButton(
                     onPressed: () {
-                      _showBlockReasonDialog();
+                      print('차단하기 클릭됨');
+                      // 차단 다이얼로그 표시 코드 추가
+                      showDialog(
+                        context: context,
+                        builder: (BuildContext context) {
+                          return AlertDialog(
+                            titlePadding: EdgeInsets.only(top: 24),
+                            contentPadding: EdgeInsets.symmetric(horizontal: 24),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(12.0),
+                            ),
+                            title: Center(
+                              child: Text(
+                                '차단 사유를 입력해주세요',
+                                style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
+                              ),
+                            ),
+                            content: SizedBox(
+                              height: 100,
+                              child: TextField(
+                                maxLines: 4,
+                                decoration: InputDecoration(
+                                  border: OutlineInputBorder(),
+                                ),
+                              ),
+                            ),
+                            actions: [
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  ElevatedButton(
+                                    onPressed: () {
+                                      Navigator.of(context).pop();
+                                    },
+                                    style: ElevatedButton.styleFrom(
+                                      foregroundColor: Colors.purple, backgroundColor: Colors.purple[100], // 글자 색상
+                                      minimumSize: Size(80, 40), // 최소 크기
+                                      shape: RoundedRectangleBorder(
+                                        borderRadius: BorderRadius.circular(8.0),
+                                      ),
+                                    ),
+                                    child: Text('확인'),
+                                  ),
+                                  SizedBox(width: 8), // 버튼 사이의 간격
+                                  ElevatedButton(
+                                    onPressed: () {
+                                      Navigator.of(context).pop();
+                                    },
+                                    style: ElevatedButton.styleFrom(
+                                      foregroundColor: Colors.purple, backgroundColor: Colors.purple[200], // 글자 색상
+                                      minimumSize: Size(80, 40), // 최소 크기
+                                      shape: RoundedRectangleBorder(
+                                        borderRadius: BorderRadius.circular(8.0),
+                                      ),
+                                    ),
+                                    child: Text('취소'),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          );
+                        },
+                      );
                     },
                     child: Text(
                       '차단하기',
@@ -648,72 +823,188 @@ class _HomePageFAState extends State<HomePageFA> with SingleTickerProviderStateM
     );
   }
 
-  void _showBlockReasonDialog() {
-    showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          titlePadding: EdgeInsets.only(top: 24),
-          contentPadding: EdgeInsets.symmetric(horizontal: 24),
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(12.0),
+  Widget _buildNoticeCard({
+    required String date,
+    required String title,
+    required String description,
+    required String likes,
+    required String views,
+  }) {
+    return InkWell(
+      onTap: () {
+        print('$title 공지 클릭됨');
+        Navigator.push(
+          context,
+          PageRouteBuilder(
+            pageBuilder: (context, animation, secondaryAnimation) =>
+                NoticeDetailPage(),
+            transitionsBuilder: (context, animation, secondaryAnimation, child) {
+              const begin = Offset(1.0, 0.0); // 오른쪽에서 시작
+              const end = Offset.zero;
+              const curve = Curves.ease;
+
+              var tween = Tween(begin: begin, end: end)
+                  .chain(CurveTween(curve: curve));
+
+              return SlideTransition(
+                position: animation.drive(tween),
+                child: child,
+              );
+            },
           ),
-          title: Text(
-            '차단 사유를 입력해주세요',
-            style: TextStyle(fontSize: 14,
-              fontWeight: FontWeight.w500),
-            textAlign: TextAlign.center,
-          ),
-          content: Container(
-            height: 100,
-            child: TextField(
-              maxLines: 4,
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-              ),
-            ),
-          ),
-          actions: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                TextButton(
-                  onPressed: () {
-                    Navigator.of(context).pop();
-                  },
-                  style: TextButton.styleFrom(
-                    padding: EdgeInsets.symmetric(horizontal: 40, vertical: 7),
-                    backgroundColor: Colors.pink[50],
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(8),
-                    ),
-                  ),
-                  child: Text(
-                    '확인',
-                    style: TextStyle(color: Colors.black),
-                  ),
-                ),
-                TextButton(
-                  onPressed: () {
-                    Navigator.of(context).pop();
-                  },
-                  style: TextButton.styleFrom(
-                    padding: EdgeInsets.symmetric(horizontal: 40, vertical: 7),
-                    backgroundColor: Colors.purple[100],
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(8),
-                    ),
-                  ),
-                  child: Text(
-                    '취소',
-                    style: TextStyle(color: Colors.black),
-                  ),
-                ),
-              ],
-            ),
-          ],
         );
       },
+      splashColor: Colors.purple.withOpacity(0.3), // 물결 효과 색상
+      child: Card(
+        elevation: 2.0,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(12.0),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(12.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                date,
+                style: TextStyle(
+                  color: Colors.grey,
+                  fontSize: 12,
+                ),
+              ),
+              SizedBox(height: 4),
+              Text(
+                title,
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 14,
+                  color: Colors.purple,
+                ),
+              ),
+              SizedBox(height: 4),
+              Text(
+                description,
+                style: TextStyle(
+                  color: Colors.grey,
+                  fontSize: 12,
+                ),
+              ),
+              SizedBox(height: 12),
+              Row(
+                children: [
+                  Icon(Icons.thumb_up, size: 16, color: Colors.purple),
+                  SizedBox(width: 4),
+                  Text(
+                    likes,
+                    style: TextStyle(color: Colors.purple, fontSize: 12),
+                  ),
+                  SizedBox(width: 16),
+                  Icon(Icons.remove_red_eye, size: 16, color: Colors.purple),
+                  SizedBox(width: 4),
+                  Text(
+                    views,
+                    style: TextStyle(color: Colors.purple, fontSize: 12),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildInquiryCard({
+    required String profileName,
+    required String inquiry,
+    required String imagePath,
+    required String status,
+  }) {
+    return InkWell(
+      onTap: () {
+        print('$profileName 문의 클릭됨');
+        Navigator.push(
+          context,
+          PageRouteBuilder(
+            pageBuilder: (context, animation, secondaryAnimation) =>
+                ComplaintDetailPage(),
+            transitionsBuilder: (context, animation, secondaryAnimation, child) {
+              const begin = Offset(1.0, 0.0); // 오른쪽에서 시작
+              const end = Offset.zero;
+              const curve = Curves.ease;
+
+              var tween = Tween(begin: begin, end: end)
+                  .chain(CurveTween(curve: curve));
+
+              return SlideTransition(
+                position: animation.drive(tween),
+                child: child,
+              );
+            },
+          ),
+        );
+      },
+      splashColor: Colors.purple.withOpacity(0.3), // 물결 효과 색상
+      child: Card(
+        elevation: 2.0,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(12.0),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(12.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                children: [
+                  CircleAvatar(
+                    radius: 24,
+                    child: Icon(Icons.person), // 프로필 이미지를 여기에 추가할 수 있음
+                  ),
+                  SizedBox(width: 10),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        profileName,
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+              SizedBox(height: 12),
+              Text(inquiry),
+              SizedBox(height: 12),
+              if (imagePath.isNotEmpty)
+                Container(
+                  width: 100,
+                  height: 60,
+                  color: Colors.pink[100],
+                  child: Center(
+                    child: Text('문의 사진', style: TextStyle(color: Colors.white)),
+                  ),
+                ),
+              SizedBox(height: 12),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Text(
+                    '처리상태 : $status',
+                    style: TextStyle(
+                      color: status == '대기중' ? Colors.purple : Colors.grey,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
